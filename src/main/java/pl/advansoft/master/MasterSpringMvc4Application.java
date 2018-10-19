600029a4e34cf2ae6f27fa540ef7ee1d9fed1333
Piotr Borelowski <piotr.borelowski@gmail.com>
2018-10-19 15:50:38 +0200

    initial commit

---
+++ src/main/java/pl/advansoft/master/MasterSpringMvc4Application.java
@@ -0,0 +1,12 @@
+package pl.advansoft.master;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasterSpringMvc4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MasterSpringMvc4Application.class, args);
+	}
+}
---
+++ src/test/java/pl/advansoft/master/MasterSpringMvc4ApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.advansoft.master;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MasterSpringMvc4ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
