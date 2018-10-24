aa1456a70763269ccf10062f17bc37b329eec8aa
Piotr Borelowski <piotr.borelowski@gmail.com>
2018-10-24 11:03:47 +0200

    now using thymeleaf

--- src/main/java/pl/advansoft/master/MasterSpringMvc4Application.java
+++ src/main/java/pl/advansoft/master/MasterSpringMvc4Application.java
@@ -2,11 +2,13 @@ package pl.advansoft.master;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class MasterSpringMvc4Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MasterSpringMvc4Application.class, args);
+		ConfigurableApplicationContext ctx = SpringApplication.run(MasterSpringMvc4Application.class, args);
+		System.out.println("count: " + ctx.getBeanDefinitionCount());
 	}
 }
--- src/main/java/pl/advansoft/master/controller/HelloController.java
+++ src/main/java/pl/advansoft/master/controller/HelloController.java
@@ -1,15 +1,19 @@
 package pl.advansoft.master.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HelloController {
 
-	@RequestMapping("/")
-	@ResponseBody
-	public String hello() {
-		return "Hejkaaa!";
+	@RequestMapping("/demo")
+	public ModelAndView hello(@RequestParam String user) {
+		ModelAndView mv = new ModelAndView("home");
+		ModelMap modelMap = mv.getModelMap();
+		modelMap.addAttribute("osoba", user);
+		return mv;
 	}
 }
