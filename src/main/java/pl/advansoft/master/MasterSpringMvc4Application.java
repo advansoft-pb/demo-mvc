13df887e48af7bd5c1cead3d19444e5a269346d4
Piotr Borelowski <piotr.borelowski@gmail.com>
2018-10-24 11:37:06 +0200

    reading ThymeleafProperties

--- src/main/java/pl/advansoft/master/MasterSpringMvc4Application.java
+++ src/main/java/pl/advansoft/master/MasterSpringMvc4Application.java
@@ -1,7 +1,12 @@
 package pl.advansoft.master;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.thymeleaf.ThymeleafProperties;
 import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
@@ -9,6 +14,16 @@ public class MasterSpringMvc4Application {
 
 	public static void main(String[] args) {
 		ConfigurableApplicationContext ctx = SpringApplication.run(MasterSpringMvc4Application.class, args);
+		Stream.of(ctx.getBeanDefinitionNames()).forEach(System.out::println);
 		System.out.println("count: " + ctx.getBeanDefinitionCount());
+
+		ThymeleafProperties thymeleafProperties = ctx.getBean(ThymeleafProperties.class);
+		System.out.println(thymeleafProperties);
+		System.out.println(thymeleafProperties.getPrefix());
+		System.out.println(thymeleafProperties.getSuffix());
+
+		String[] names = ctx.getBeanNamesForType(ThymeleafProperties.class);
+		List<String> list = Arrays.asList(names);
+		System.out.println(list);
 	}
 }
--- src/main/java/pl/advansoft/master/controller/HelloController.java
+++ src/main/java/pl/advansoft/master/controller/HelloController.java
@@ -1,19 +1,22 @@
 package pl.advansoft.master.controller;
 
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HelloController {
 
 	@RequestMapping("/demo")
-	public ModelAndView hello(@RequestParam String user) {
-		ModelAndView mv = new ModelAndView("home");
-		ModelMap modelMap = mv.getModelMap();
-		modelMap.addAttribute("osoba", user);
-		return mv;
+	public String hello(Model model, @RequestParam(required = false, defaultValue = "Użytkownik") String user) {
+		model.addAttribute("osoba", user);
+
+		Map<String, Object> map = model.asMap();
+		System.out.println("size: " + map.size());
+
+		return "home";
 	}
 }
